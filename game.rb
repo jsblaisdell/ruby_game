e1aef8b169c98ff2e8fe5a406c0d8009a8861df3
MakerSquare Student <students@themakersquare.com>
2013-09-04 17:40:12 -0500

    changed some text and added a bit more to the orc fight

--- game.rb
+++ game.rb
@@ -5,7 +5,7 @@ class Character
         @str = str
         @magic = magic
     end
-    def attack
+    def stab
         rand(10) + @str / 2
     end
     def spell
@@ -23,19 +23,38 @@ end
 class Enemy < Character
 end
 
+block = "================================================"
+
 player = Player.new(100, 20, 20)
 orc1 = Enemy.new(30, 10, 0)
 orc2 = Enemy.new(30, 10, 0)
 
-puts "You are in a bar. Two orcs are looking at you from across the room. They don't look nice."
-puts "Type: [attack] or [leave]"
+puts block
+puts "You're sitting in a dark bar with a pint of mead. Two orcs glare at you from across the room; they don't look nice."
+puts "Type: [attack], [cast] or [leave]"
+puts block
 
 choice = gets.chomp
+puts
 
 if choice == "leave"
     puts "How boring."
 elsif choice == "attack"
-    orc1.take_damage(player.attack)
+    orc1.take_damage(player.stab)
+    puts "That bastard is down to #{orc1.hp} health!"
+elsif choice == "cast"
+    orc1.take_damage(player.spell)
+    puts "That bastard is down to #{orc1.hp} health!"
 else
-    puts "You didn't follow the directions."
+    puts "You're terrible at following directions. Type one of the words between the square brackets."
 end
+
+puts
+
+puts "The first orc attacked you!"
+player.take_damage(orc1.stab)
+puts
+puts "The second orc attacked you!"
+player.take_damage(orc2.stab)
+puts
+puts "you are down to #{player.hp} health!"
